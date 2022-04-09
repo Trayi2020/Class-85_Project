54cdbbe7d0e552d3c095b5e01813b6485ea2159c
Trayi <89193073+Trayi2020@users.noreply.github.com>
2022-04-09 14:21:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+car_width = 150;
+car_height = 120;
+
+background_image = "parking";
+car_image = "car";
+
+
+car_x = 5;
+car_y= 225;
+
+function add() {
+	background_imgTag = new Image(); 				
+	background_imgTag.onload = uploadBackground; 	
+	background_imgTag.src = background_image;   	
+
+	car_imgTag = new Image(); 				
+	car_imgTag.onload = uploadcar; 	
+	car_imgTag.src = car_image;  		 
+
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadcar() {
+	ctx.drawImage(car_imgTag, car_x,car_y, car_width, car_height);
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if(car_y >=0)
+	{
+		car_y = car_y - 10;
+		console.log("When up arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		 uploadBackground();
+		 uploadcar();
+		
+	}
+}
+
+function down()
+{
+	if(car_y <=350)
+	{
+		car_y =car_y+ 10;
+		console.log("When down arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		uploadBackground();
+		uploadcar();
+		
+	}
+}
+
+function left()
+{
+	if(car_x >= 0)
+	{
+		car_x =car_x - 10;
+		console.log("When left arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		uploadBackground();
+		 uploadcar();
+		
+	}
+}
+
+function right()
+{
+	if(car_x <= 750)
+	{
+		car_x =car_x + 10;
+		console.log("When right arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		uploadBackground();
+		uploadcar();
+		
+   }
+}
